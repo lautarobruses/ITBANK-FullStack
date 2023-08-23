ec9cfcf19216b3fb4e994f206e42dab36fba0f7b
Ivan Lezcano <lezcamati@gmail.com>
2023-08-23 17:49:32 -0300

    pagos js

---
+++ scripts/pagos.js
@@ -0,0 +1,6 @@
+/*Share redes */
+const share=document.querySelector(".share");
+const toggle=document.querySelector(".toggle");
+toggle.addEventListener("click",()=>{
+  share.classList.toggle("active");
+})
