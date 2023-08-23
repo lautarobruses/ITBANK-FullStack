bd4a44d6c025874951158e5609993db5f718f8b0
Ivan Lezcano <lezcamati@gmail.com>
2023-08-22 22:29:18 -0300

    Agregar share 360 / linkear index

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 const share=document.querySelector(".share");
 const toggle=document.querySelector(".toggle");
 toggle.addEventListener("click", () => {
-  share.classList.toggle("active");});
+  share.classList.toggle("active");
+});
