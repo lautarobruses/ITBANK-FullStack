5eb61040a4fab6d9b05de1f2fdac4470de6d660b
Ivan Lezcano <lezcamati@gmail.com>
2023-08-23 18:28:08 -0300

    Desplegar comprobantes js

--- scripts/pagos.js
+++ scripts/pagos.js
@@ -4,3 +4,22 @@ const toggle=document.querySelector(".toggle");
 toggle.addEventListener("click",()=>{
   share.classList.toggle("active");
 })
+/*DESPLEGAR*/
+
+const botonServicio = document.getElementById("servicio");
+const comprobanteDiv = document.getElementById("comprobantes");
+
+const estadoGuardado = localStorage.getItem("comprobanteVisible");
+if (estadoGuardado === "true") {
+    comprobanteDiv.style.display = "block";
+}
+
+botonServicio.addEventListener("click", function() {
+    if (comprobanteDiv.style.display === "block") {
+        comprobanteDiv.style.display = "none";
+        localStorage.setItem("comprobanteVisible", "false");
+    } else {
+        comprobanteDiv.style.display = "block";
+        localStorage.setItem("comprobanteVisible", "true");
+    }
+});
