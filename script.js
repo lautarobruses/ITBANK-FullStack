f2563e7477295b24b3173def8d6fad091881cd0e
lautarobruses <lautarobruses@gmail.com>
2023-08-24 19:19:44 -0300

    .

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const toggleButton = document.getElementById("button-menu")
 const navWrapper = document.getElementById("nav")
 
-console.log(document);
-console.log(toggleButton);
-console.log(navWrapper);
-
 toggleButton.addEventListener('click',() => {
   toggleButton.classList.toggle('close')
   navWrapper.classList.toggle('show')
