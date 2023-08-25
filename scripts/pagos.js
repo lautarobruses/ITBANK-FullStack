f21f9b2e3f21145eb49ccaf660ee75ba754bb69b
Ivan Lezcano <lezcamati@gmail.com>
2023-08-25 18:22:33 -0300

    desplazamiento js

--- scripts/pagos.js
+++ scripts/pagos.js
@@ -6,22 +6,11 @@ toggle.addEventListener("click",()=>{
 })
 
 /*DESPLEGAR*/
-const botonServicio = document.getElementById("servicio");
-const comprobanteDiv = document.getElementById("comprobantes");
-
-const estadoGuardado = localStorage.getItem("comprobanteVisible");
-if (estadoGuardado === "true") {
-    comprobanteDiv.style.maxHeight = "500px"; 
-} else {
-    comprobanteDiv.style.maxHeight = "0"; 
-}
-
-botonServicio.addEventListener("click", function() {
-    if (comprobanteDiv.style.maxHeight === "0px") {
-        comprobanteDiv.style.maxHeight = "500px"; 
-        localStorage.setItem("comprobanteVisible", "true");
+function desplegarArticulos() {
+    const comprobantesHistoria = document.getElementById("comprobantes");
+    if (comprobantesHistoria.style.display === "none") {
+      comprobantesHistoria.style.display = "grid";
     } else {
-        comprobanteDiv.style.maxHeight = "0"; 
-        localStorage.setItem("comprobanteVisible", "false");
+      comprobantesHistoria.style.display = "none";
     }
-});
+}
