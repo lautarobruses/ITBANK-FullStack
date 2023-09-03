4370de8907e6713404721809b132599ea3b2b0c7
lautarobruses <lautarobruses@gmail.com>
2023-09-02 21:51:18 -0300

    Background.js

--- sprint-2/src/App.js
+++ sprint-2/src/App.js
@@ -3,27 +3,13 @@ import {
     Routes, Route
 } from 'react-router-dom'
 
-import { styled } from 'styled-components'
+import GlobalStyles from './components/GlobalStyles'
 
-import GlobalStyles from './GlobalStyles'
 
-import Footer from './components/Footer/Footer'
+import Background from './components/Background'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
-
-import background from '../src/assets/background.jpg'
-
-const Background = styled.div`
-    position: fixed;
-    top: 10%;
-    left: 15%;
-    width: 100%;
-    height: 90%;
-    background-image: url(${background});
-    background-size: cover;
-    background-position: center;
-    z-index: -1;
-`
+import Footer from './components/Footer/Footer'
 
 const App = () => {
     return (
---
+++ sprint-2/src/components/Background.js
@@ -0,0 +1,21 @@
+import { styled } from 'styled-components'
+
+import background from '../assets/background.jpg'
+
+const StyledDiv = styled.div`
+    position: fixed;
+    top: 10%;
+    left: 15%;
+    width: 100%;
+    height: 90%;
+    background-image: url(${background});
+    background-size: cover;
+    background-position: center;
+    z-index: -1;
+`
+
+const Background = () => {
+    return <StyledDiv />
+}
+
+export default Background
