2baf641fe1d26541fc188e13039e7a3a618e457e
lautarobruses <lautarobruses@gmail.com>
2023-09-07 02:00:33 -0300

    .

--- sprint-2/src/App.js
+++ sprint-2/src/App.js
@@ -5,32 +5,32 @@ import {
 import GlobalStyles from './components/GlobalStyles'
 
 import Background from './components/Background'
-import Header from './components/Header/Header'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import Main from './components/Main/Main'
-// import Login from './components/Login/Login'
+// import Header from './components/Header/Header'
+// import Navbar from './components/Navbar/Navbar'
+// import Footer from './components/Footer/Footer'
+// import Main from './components/Main/Main'
+import Login from './components/Login/Login'
 
 
 const App = () => {
-    return (
-        <Router>
-            <GlobalStyles /> {/*No funciona en React Native*/}
-            <Background />
-            <Header />
-            <Navbar />
-            <Main />
-            <Footer />
-        </Router>
-    )
-
     // return (
     //     <Router>
     //         <GlobalStyles /> {/*No funciona en React Native*/}
     //         <Background />
-    //         <Login />
+    //         <Header />
+    //         <Navbar />
+    //         <Main />
+    //         <Footer />
     //     </Router>
     // )
+
+    return (
+        <Router>
+            <GlobalStyles /> {/*No funciona en React Native*/}
+            <Background />
+            <Login />
+        </Router>
+    )
 }
 
 export default App
--- sprint-2/src/components/Login/styles/StyledBackgroundForm.js
+++ sprint-2/src/components/Login/styles/StyledBackgroundForm.js
@@ -4,6 +4,7 @@ const StyledBackgroundForm = styled.div `
     background-color: var(--dark-sky-blue);
     opacity: 0.8;
     border-radius: 0 0 64px 64px;
+    margin-bottom: 32px;
 
     @media screen and (max-width: 1023px) {
         border-radius: 0 0 32px 32px;
