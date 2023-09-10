c3d45e2a3621c3f03849f8888ebb34d2a66e34d3
Ivan Lezcano <lezcamati@gmail.com>
2023-09-10 15:30:16 -0300

    Cuenta modific

--- sprint-2/src/components/Main/Cuenta.js
+++ sprint-2/src/components/Main/Cuenta.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UCA from '../../assets/UCA.png'
 import USD from '../../assets/USD.png'
 import './estilosMain/Cuenta.css'
 
 const Cuenta = () => {
+    const [pesos] = useState({
+        monto:45246,
+        numCuenta:23762668920802,
+    })
+    const [dolar] = useState({
+        monto:727,
+        numCuenta:55684497220905,
+    })
+    // const comprarDolares = () => {
+    //     const valorDolar = 710
+    //     const cantidadDolares = prompt('Ingrese la cantidad que desea comprar: ')
+    //     const totalCompra = valorDolar * cantidadDolares
+    //     if (pesos.monto >= totalCompra && cantidadDolares > 0){
+    //         setPesos({ pesos, monto: pesos.monto - totalCompra })
+    //         setDolar({ dolar, monto: dolar.monto + parseInt(cantidadDolares) })
+    //     } else {
+    //         alert('Saldo insuficiente para comprar dólares.')
+    //     }
+    // }
+    // const venderDolares = () => {
+    //     const valorDolar = 710
+    //     const cantidadDolares = prompt('Ingrese la cantidad de dólares que desea vender: ')
+    //     if(dolar.monto >= cantidadDolares && cantidadDolares > 0){
+    //         const totalVenta = valorDolar * cantidadDolares
+    //         setDolar({ dolar, monto: dolar.monto - parseInt(cantidadDolares) })
+    //         setPesos({ pesos, monto: pesos.monto + totalVenta })
+    //     } else {
+    //         alert('Saldo insuficiente para vender dólares')
+    //     }
+    // }
+
     return (
         <div className='container'>
             <h1>Tus cuentas</h1>
@@ -14,17 +45,17 @@ const Cuenta = () => {
                         <h6 className='izquierda'>PRINCIPAL</h6>
                         <img src={UCA} alt='Caja de Ahorro' />
                         <h4 className='cardTitle'>Caja Ahorro Pesos</h4>
-                        <h2 className="monto">$45.244,20</h2>
-                        <p className='numCuenta'>N° 237626689-2 080-1</p>
+                        <h2 className="monto">$ {pesos.monto}</h2>
+                        <p>{pesos.numCuenta}</p>
                     </div>
                     <div className='card'>
                         <br />
                         <img src={USD} alt='Caja de Ahorro en dólares' />
                         <h4 className="cardTitle">Caja Ahorro Dólares</h4>
-                        <h2 className="monto">U$D 726,00</h2>
-                        <p className="numCuenta">N° 556844972-2 090-5</p>
-                        <button className="button">Comprar</button>
-                        <button className="button">Vender</button>
+                        <h2 className="monto">U$D {dolar.monto}</h2>
+                        <p>{dolar.numCuenta}</p>
+                        {/* <button className="button" onClick={comprarDolares}>Comprar</button>
+                        <button className="button" onClick={venderDolares}>Vender</button> */}
                     </div>
                 </div>
                 <h3 className="subtitle">Cheques</h3>
