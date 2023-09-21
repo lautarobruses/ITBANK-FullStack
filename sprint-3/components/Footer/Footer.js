896883ec38818fc49ef638a3568871ddbb7c72f4
lautarobruses <lautarobruses@gmail.com>
2023-09-20 22:45:21 -0300

    start ContactForm.js

--- sprint-3/components/Footer/Footer.js
+++ sprint-3/components/Footer/Footer.js
@@ -3,13 +3,17 @@ import styles from '@/styles/Footer/Footer.module.css'
 import Media from './Media'
 import Contact from './Contact'
 import Terms from './Terms'
+import ContactForm from './ContactForm'
 
 const Footer = () => {
     return (
         <footer className={`${styles.footer}`}>
-            <Media />
-            <Contact />
-            <Terms />
+            <div className={`${styles.leftContainer}`}>
+                <Media />
+                <Contact />
+                <Terms />
+            </div>
+            <ContactForm />
         </footer>
     )
 }
