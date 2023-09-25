908592f0fad0843c536d87080453b4ca7ab48f05
Ivan Lezcano <lezcamati@gmail.com>
2023-09-24 23:21:03 -0300

    Contextprovider transfer

---
+++ sprint-3/contexts/TransferContext.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { createContext } from 'react'
+
+const FormContext= createContext()
+
+const FormProvider = ({ children }) => {
+    const [formulario, setFormulario] = useState({
+        addressee:'',
+        motivo: '',
+        amount: 0,
+    })
+
+
+    return (
+        <FormContext.Provider value={{formulario, setFormulario}}>
+            {children}
+        </FormContext.Provider>
+    )
+}
+
+export default {FormProvider, FormContext} 
