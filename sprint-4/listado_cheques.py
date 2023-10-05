1891e8a0d24a1f0117a537e0ef877e67a9b4d92e
Maria Laura Morales <marialauramoralevillaga@gmail.com>
2023-10-05 20:26:22 -0300

    name of salida.csv changed

--- sprint-4/listado_cheques.py
+++ sprint-4/listado_cheques.py
@@ -122,7 +122,7 @@ def printNumpy(table:np, head:list|None):
     for list in table:
         print( sep.join([ x.center(11) for x in map( str, list ) ]) )
 
-def saveCsv(table:np, head:list|None):
+def saveCsv(table:np, head:list|None, dni:int):
     '''Guarda el numpy dado en un archivo salida.csv.'''
     sep= ","
     strignTable = sep.join(map( str, head ))
@@ -130,7 +130,9 @@ def saveCsv(table:np, head:list|None):
     for list in table:
         strignTable = strignTable + '\n' + sep.join(map( str, list ))
 
-    with open('data/salida.csv', 'w') as file: #nombre archivo = <DNI><TIMESTAMP_ACTUAL>.csv
+    timestamp = int(datetime.timestamp(datetime.now()))
+  
+    with open(f'{dni}_{timestamp}.csv', 'w') as file: #nombre archivo = <DNI><TIMESTAMP_ACTUAL>.csv
         file.write(strignTable)
         file.close()
 
@@ -173,6 +175,7 @@ def containsArgs(list, *args): #Estado: Puede tener 3 valores pendiente, aprobad
     return True
 
 #CODIGO PRINCIPAL
+
 def main():
     if len(sys.argv) < 5:
         muestraMensajeError()
@@ -219,8 +222,7 @@ def main():
             if ( salida == 'PANTALLA' ):
                 printNumpy(leakedData, head)
             else:
-                saveCsv(leakedData, head)  
+                saveCsv(leakedData, head, dni)  
         else:
             muestraMensajeError()
-
 main() 
