ebfdcb7fcabe9f9a50402a42de9c62d91024cbf4
Maria Laura Morales <marialauramoralevillaga@gmail.com>
2023-10-19 13:35:40 -0300

    sprint-5

---
+++ sprint-5/package/cliente.py
@@ -0,0 +1,19 @@
+class Cliente:
+    def __init__(self, numero, nombre, apellido, dni, transacciones):
+        self.numero = numero
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
+        self.transacciones = transacciones
+
+class Classic(Cliente):
+    def __init__(self,):
+        self
+
+class Gold(Cliente):
+    def __init__(self,):
+        self
+
+class Black(Cliente):
+    def __init__(self,):
+        self
