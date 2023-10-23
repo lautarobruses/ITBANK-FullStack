13135079958563bbb3108d747959e6a141c79bb5
Ivan Lezcano <lezcamati@gmail.com>
2023-10-22 23:03:24 -0300

    bool => string

--- sprint-5/cliente/cliente.py
+++ sprint-5/cliente/cliente.py
@@ -81,78 +81,93 @@ class Cliente:
     def alta_cuenta_inversion(self, transaccion) -> bool:
         '''descripcion'''
         if transaccion.tipo == 'CLASSIC':
-            return False # no tiene acceso a la cuenta de inversion 
+            razon = 'RECHAZADA La cuenta Classic no tiene permitido el acceso a las inversiones'
+            return razon # no tiene acceso a la cuenta de inversion 
         else: 
             self.cuenta_inversion = transaccion
             monto_plazo_fijo = self.calcular_monto_plazo_fijo(transaccion)
             transaccion.monto_plazo_fijo = monto_plazo_fijo
-            return True
+            razon1 = 'Aceptada, tu cuenta cumple los requisitos para acceder a las inversiones'
+            return razon1
+        
         
 
 
 
-    def comprar_dolar(self, monto) -> bool:
+    def comprar_dolar(self, transaccion) -> bool:
         '''Compra una cantidad de dólares y devuelve el monto en pesos o False si la compra falla.'''
         if self.caja_ahorro_dolar:
-            costo_en_pesos = self.calcular_monto_total(precio_dolar_oficial, monto)
+            costo_en_pesos = self.calcular_monto_total(precio_dolar_oficial, transaccion.monto)
             
-            if costo_en_pesos > self.saldo_disponible_en_cuenta:
-                return False  # No hay suficientes fondos en pesos para la compra de dólares
-
-            return costo_en_pesos  # Devuelve el costo en pesos de la compra
+            if costo_en_pesos > transaccion.saldoDisponibleEnCuenta:
+                razon = 'RECHAZADA, no hay suficientes fondos para la compra' 
+                return razon # No hay suficientes fondos en pesos para la compra de dólares
+            razon1 = f'Aceptada, tus fondos: {costo_en_pesos} los tenes disponibles para la compra' 
+            return razon1  # Devuelve el costo en pesos de la compra
         else:
-            return False
+            razon2 = 'RECHAZADA, No contas con una caja de ahorro en dólares'
+            return razon2
 
-    def vender_dolar(self, monto) -> bool:
+    def vender_dolar(self, transaccion) -> bool:
         '''Vende una cantidad de dólares y devuelve el monto en pesos o False si la venta falla.'''
         if self.caja_ahorro_dolar:
-            if monto > self.saldo_disponible_en_cuenta:
-                return False  # No hay suficientes dólares para la venta
+            if transaccion.monto > transaccion.saldoDisponibleEnCuenta:
+                razon = 'RECHAZADA, No hay suficientes dólares para la venta'
+                return razon  # No hay suficientes dólares para la venta
             
-            monto_en_pesos = self.calcular_monto_total(precio_dolar_oficial, monto)
-            
-            return monto_en_pesos  # Devuelve el monto en pesos de la venta
+            monto_en_pesos = self.calcular_monto_total(precio_dolar_oficial, transaccion.monto)
+            razon1 = f'Aceptada, contas con : {monto_en_pesos} para la venta'
+            return razon1 # Devuelve el monto en pesos de la venta
         else:
-            return False
+            razon2 = 'RECHAZADA, No contas con una caja de ahorro en dólares'
+            return razon2
 
-    def transferencia_enviada_pesos(self, monto, cuenta_destino, es_transferencia_enviada=True):
+    def transferencia_enviada_pesos(self, transaccion, cuenta_destino, es_transferencia_enviada=True):
         '''descripcion'''
-        if monto > self.saldo_disponible_en_cuenta:
-            return False
+        if transaccion.monto > transaccion.saldoDisponibleEnCuenta:
+            razon = 'RECHAZADA, el monto proporcionado es mayor que tu saldo disponible '
+            return razon
         
-        monto_con_comision = fn.descontar_comision(monto, self.porcentaje_comision_envio)
+        monto_con_comision = fn.descontar_comision(transaccion.monto, self.porcentaje_comision_envio)
         if cuenta_destino.transferencia_recibida_pesos(monto_con_comision, self):
-            self.saldo_disponible_en_cuenta -= monto_con_comision
-            return True
+            transaccion.saldoDisponibleEnCuenta -= monto_con_comision
+            razon2 = 'Aceptada, el saldo está disponible para ser enviado'
+            return razon2
         
-    def transferencia_enviada_dolares(self, monto, cuenta_destino, es_transferencia_enviada=True):
+    def transferencia_enviada_dolares(self, transaccion, cuenta_destino, es_transferencia_enviada=True):
         '''descripcion'''
         if not self.caja_ahorro_dolar:
-            return False # No se permita la transferencia en dolar
+            razon = 'RECHAZADA, No contas con una caja de ahorro en dólares'
+            return razon # No se permita la transferencia en dolar
         
-        if monto > self.saldo_disponible_en_cuenta:
-            return False # No hay suficientes dolares para la transferencia
+        if transaccion.monto > transaccion.saldoDisponibleEnCuenta:
+            razon1 = 'REZHADA, El monto proporcionado es mayor que el saldo disponible'
+            return razon1  # No hay suficientes dolares para la transferencia
         
-        monto_en_pesos = monto * precio_dolar_oficial
+        monto_en_pesos = transaccion.monto * precio_dolar_oficial
         monto_con_comision = fn.descontar_comision(monto_en_pesos, self.porcentaje_comision_envio)
 
         if cuenta_destino.transferencia_recibida_dolares(monto_con_comision, self):
-            self.saldo_disponible_en_cuenta -= monto_con_comision
-            return True
+            transaccion.saldoDisponibleEnCuenta -= monto_con_comision
+            razon2 = 'Aceptada, contas con saldo disponible y cuenta de ahorro en dólares'
+            return razon2
 
-    def transferencia_recibida_pesos(self, monto, es_transferencia_enviada=False):
+    def transferencia_recibida_pesos(self, transaccion, es_transferencia_enviada=False):
         '''descripcion'''
-        monto_con_comision = fn.descontar_comision(monto, self.porcentaje_comision_recibo)
-        self.saldo_disponible_en_cuenta += monto_con_comision
-        return True
+        monto_con_comision = fn.descontar_comision(transaccion.monto, self.porcentaje_comision_recibo)
+        transaccion.saldoDisponibleEnCuenta += monto_con_comision
+        razon = 'Aceptada, la transferencia fue exitosa.'
+        return razon
 
-    def transferencia_recibida_dolares(self, monto, es_transferencia_enviada=False):
+    def transferencia_recibida_dolares(self, transaccion, es_transferencia_enviada=False):
         '''descripcion'''
         if not self.caja_ahorro_dolar:
-            return False  # No se permite la recepción de dólares
+            razon = 'Rechazada, No se permite la recepción de dólares. No contas con caja de ahorro.'
+            return razon  # No se permite la recepción de dólares
 
         # Realiza la recepción de la transferencia en dólares
-        monto_en_pesos = monto * precio_dolar_oficial
+        monto_en_pesos = transaccion.monto * precio_dolar_oficial
         monto_con_comision = fn.descontar_comision(monto_en_pesos, self.porcentaje_comision_recibo)
-        self.saldo_disponible_en_cuenta += monto_con_comision
-        return True 
+        transaccion.saldoDisponibleEnCuenta += monto_con_comision
+        razon1 = 'Aceptada, contás con caja de ahorro en dólares para recibir el dinero,'
+        return razon1 
