13683ebf23d41a69bce716128cd45f3bb3eea53c
Maria Laura Morales <marialauramoralevillaga@gmail.com>
2023-11-19 20:44:02 -0300

    views.py cuenta

--- sprint-7/itbank/cuenta/views.py
+++ sprint-7/itbank/cuenta/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import LoginForm, RegisterForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -7,4 +8,12 @@ def login(request):
     return render(request, 'cuenta/login.html', {'form': LoginForm})
 
 def register(request):
-    return render(request, 'cuenta/register.html', {'form': RegisterForm})
+    if(request.method == 'GET'):
+        return render(request, 'cuenta/register.html', {'form': RegisterForm})
+    else:
+        try:
+            user = User.objects.create_user(
+                username = request.POST['']
+            )
+        except:
+            print("hola")
