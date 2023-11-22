9b38415ac3904ee7591d741dc55340c2949fdc1a
Ivan Lezcano <lezcamati@gmail.com>
2023-11-22 00:41:39 -0300

    forms pagos 60%

--- sprint-7/itbank/pagos/views.py
+++ sprint-7/itbank/pagos/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render
-from base.forms import ContactForm
+from django.shortcuts import render, redirect
+from .forms import ServicioForm
+from django.contrib.sessions.models import Session
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def pagos(request):
-    form = ContactForm()
+    form = ServicioForm()
+    servicios = []
+
+    if 'servicios' in request.session:
+        servicios = request.session['servicios']
+
+    context = {
+        "form": form,
+        "servicios": servicios
+    }
+
+    return render(request, 'pagos/pagos.html', context)
+
+@login_required
+def servicios(request):
+    if request.method == 'POST':
+        form = ServicioForm(request.POST)
+        if form.is_valid():
+            request.session['servicios'] = form.cleaned_data
+            return redirect('pagos')
+    else:
+        form = ServicioForm()
 
     context = {
         "form": form
     }
 
-    return render(request, 'pagos/pagos.html', context)
-
-def servicios(request):
-    
-    return render(request, 'pagos/servicios.html')
+    return render(request, 'pagos/servicios.html', context)
